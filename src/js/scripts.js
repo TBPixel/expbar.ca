e6f99c94b16127f1208e433ee3c3bf5ea46e4e50
Tony Barry <tonympbarry@gmail.com>
2016-08-27 11:14:43 -0700

    Adds Location Section - Google Maps API

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,38 @@
 // JQuery DOM Ready Initialzie
-$(function() {
+$( function () {
     'use strict';
 
+    // Maps are inactive initially
+    var $locDOM = $( '.location' );
+    var $mapDOM = $locDOM.find ( '#map' );
+
+
+    function setMapState ( v ) {
+
+        v = v || null;
+
+        var css,
+            state = $mapDOM.css( 'pointer-events' );
+
+
+        if ( typeof v === 'string' )
+        {
+            css = v;
+        }
+        else
+        {
+            css = (state === 'none') ? 'auto' : 'none';
+        }
+
+        $mapDOM.css ( 'pointer-events', css );
+    }
+
+
+    $( document ).on ( 'click', function () { setMapState ( 'none' ); });
+
+    $locDOM.on( 'click', function ( e ) {
+
+        e.stopPropagation ();
+        setMapState ();
+    });
 });
